e87dfc2082db90405c97d4b7eba2c15ec7d25240
zoobestik <kb.chernenko@gmail.com>
2015-05-21 21:08:58 +0300

    Remove hard reference to configs/common.js from development config

--- configs/development/node.js
+++ configs/development/node.js
@@ -1,7 +1,13 @@
 'use strict';
-let common = require('../common');
+let secret = 'secret';
+
+try {
+	secret = require('../common').secret;
+} catch(e) {
+	console.warn('common file is broken');
+}
 
 module.exports = {
     port: 3000,
-    secret: common.secret
+    secret: secret
 };
