5ae72eb60c43c141d8c975c6b0e02ea8bc9f1683
zoobestik <kb.chernenko@gmail.com>
2015-06-27 12:29:07 +0300

    Add JSCS validation with pre-commit hook

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 'use strict';
 
 /* global process */
+
 const gulp = require('gulp');
 const symlink = require('gulp-symlink');
 const webpack = require('webpack-stream');
 const del = require('del');
-
+const jscs = require('gulp-jscs');
 
 gulp.task('link:app', function() {
     return gulp.src('./app')
@@ -29,10 +30,33 @@ gulp.task('link:appConfigs', function() {
         .pipe(symlink('./app/configs', { force: true }));
 });
 
-gulp.task('postinstall', [ 'link:app', 'link:components', 'link:currentConfig', 'link:appConfigs' ]);
+gulp.task('link:git-hooks', function() {
+    return gulp.src([ '.git-hooks/pre-commit', '.git-hooks/post-merge' ])
+        .pipe(symlink([ '.git/hooks/pre-commit', '.git/hooks/post-merge' ], {
+            relative: true,
+            force: true
+        }));
+});
+
+gulp.task('postinstall', [
+    'link:app',
+    'link:components',
+    'link:currentConfig',
+    'link:appConfigs',
+    'link:git-hooks',
+]);
 
 gulp.task('clean:artifacts', function(cb) {
     del([ 'node_modules', 'logs' ], cb);
 });
 
 gulp.task('clean', [ 'clean:artifacts' ]);
+
+gulp.task('lint:jscs', function() {
+    return gulp.src([ '**/*.js', '**/*.jsx' ])
+        .pipe(jscs());
+});
+
+gulp.task('lint', [ 'lint:jscs' ]);
+
+gulp.task('test', [ 'lint' ]);
