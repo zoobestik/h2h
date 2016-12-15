159b1147342a5a6c9b1cf002db4ae68e1ffe20e1
zoobestik <kb.chernenko@gmail.com>
2016-12-15 21:12:27 +0300

    Add .gz files generator in production mode

--- scripts/webpack/production.js
+++ scripts/webpack/production.js
@@ -1,6 +1,7 @@
 import { optimize } from 'webpack';
 import csso from 'postcss-csso';
 import BabiliPlugin from 'babili-webpack-plugin';
+import CompressionPlugin from 'compression-webpack-plugin';
 import base, { pluginOptions } from './common';
 
 const { OccurrenceOrderPlugin } = optimize;
@@ -12,10 +13,17 @@ export default {
     ...base,
 
     plugins: [].concat(
-        [
-            new BabiliPlugin(),
-            new OccurrenceOrderPlugin(),
-        ],
+        new BabiliPlugin(),
+        new OccurrenceOrderPlugin(),
+
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'zopfli',
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8,
+        }),
+
         base.plugins,
     ),
 };
